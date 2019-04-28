176fd7129b75d720b8306bf099ea2c863418c2fa
ahmadfahmip <ahmad.fahmi.pratama@gmail.com>
2019-04-29 01:37:35 +0700

    Add CardPrefs

---
+++ src/components/card/CardPrefs.vue
@@ -0,0 +1,35 @@
+<template>
+  <router-link :to="link">
+    <div class="card_prefs">
+      <div class="content">
+        <div class="header">
+          <h1 class="name">{{ name }}</h1>
+        </div>
+      </div>
+      <img :src="image" alt="">
+    </div>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: 'CardPrefs',
+  props: {
+    name: String,
+    image: String,
+    id: String
+  },
+  data () {
+    return {
+      link: ''
+    }
+  },
+  created () {
+    this.link = `/place/${this.id}`
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
