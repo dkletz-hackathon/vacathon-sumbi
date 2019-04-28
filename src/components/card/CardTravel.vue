7632216dc2b9d503f9ea3fce87fa6ad21a15d4f1
ahmadfahmip <ahmad.fahmi.pratama@gmail.com>
2019-04-29 00:44:19 +0700

    Add CardTravel

---
+++ src/components/card/CardTravel.vue
@@ -0,0 +1,37 @@
+<template>
+  <router-link :to="link">
+    <div class="card_travel">
+      <div class="content">
+        <p class="distance">{{ distance }}</p>
+        <div class="header">
+          <h1 class="name">{{ name }}</h1>
+        </div>
+      </div>
+      <img :src="image" alt="">
+    </div>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: 'CardTravel',
+  props: {
+    distance: String,
+    name: String,
+    image: String,
+    id: String
+  },
+  data () {
+    return {
+      link: ''
+    }
+  },
+  created () {
+    this.link = `/place/${this.id}`
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
