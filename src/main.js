e473785f3a987e0be97de238c59220fa2caa5175
Gery Wahyu <gerywahyunugraha@gmail.com>
2019-04-28 16:59:21 +0700

    Add store

--- src/main.js
+++ src/main.js
@@ -5,10 +5,13 @@ import store from './store'
 import './registerServiceWorker'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import Vue from 'vue'
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false
 
 Vue.use(Vuetify)
+Vue.use(Vuex)
 
 new Vue({
   router,
