632325da0902123df10f3e5bdc2cdcdb07bb665b
ahmadfahmip <ahmad.fahmi.pratama@gmail.com>
2019-04-29 03:54:08 +0700

    Redesign Login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,34 +1,99 @@
 <template>
-  <div class="login">
-    <div class="expandable"></div>
-    <component :is='comp'/>
-    <div class="expandable"></div>
+  <div class="login main_content justify">
+    <h1 class="main_title">Welcome to Indite!</h1>
+    <v-form
+      ref="form"
+      v-model="valid"
+      lazy-validation
+    >
+      <v-text-field
+        label="Username"
+        outline
+        required
+        class="login_input"
+        :rules="usernameRules"
+        v-model="username"
+        prepend-inner-icon="person"
+      ></v-text-field>
+      <v-text-field
+        label="Password"
+        outline
+        required
+        class="login_input"
+        :rules="passwordRules"
+        v-model="password"
+        type="password"
+        prepend-inner-icon="lock"
+      ></v-text-field>
+    </v-form>
+    <button
+      @click="login"
+    >
+      Login
+    </button>
   </div>
 </template>
 
 <script>
-import LoginBox from "@/components/@Login/LoginBox.vue";
-
 export default {
   name: 'Login',
-  components: {
-    LoginBox
-  },
   data () {
     return {
-      comp: 'LoginBox'
+      comp: 'LoginBox',
+      username: '',
+      password: '',
+      valid: true,
+      usernameRules: [
+        v => !!v || 'Name is required'
+      ],
+      passwordRules: [
+        v => !!v || 'Password is required'
+      ],
     }
+  },
+  methods: {
+    login () {
+      if (this.validate()) {
+        let username = this.username;
+        let password = this.password;
+
+        this.$store.dispatch("auth/login", { username, password }, { root: true })
+          .then(() => {
+            console.log("Login success");
+            this.$router.push("/");
+          })
+          .catch(err => console.log(err));
+      }
+    },
+    validate () {
+      if (this.$refs.form.validate()) {
+        this.snackbar = true
+        return true
+      }
+      return false
+    },
   }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 .login {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-.expandable {
-  flex-basis: 1;
+  height: 100vh;
+  padding-top: 15vh;
+  .login_input {
+    margin: 0;
+  }
+  button {
+    color: white;
+    background-color: #F99704;
+    width: 100%;
+    padding: 2.5vh 4vh;
+    border-radius: 0.5rem;
+    transition-duration: 0.2s;
+
+    &:active {
+      background-color: rgb(207, 124, 0);
+    }
+  }
 }
 </style>
